81485de596eb7cc3a76534d80a49dd3fa8a45ee9
najm <nagm113nagm@gmail.com>
2025-07-22 11:08:09 +0300

    Refactor menu input handling to use string comparison and improve error messaging

--- menu.py
+++ menu.py
@@ -14,24 +14,27 @@ menu_text = (
 
 def run_menu():
     welcome_message()
-    choice = int(input(menu_text))
-    while choice != 3:
+
+    choice = input(menu_text)
+    while choice not in "3":
         match choice:
-            case 1:
+            case "1":
                 if isApiKeyAvailable():
                     sub_translate.handle()
                 else:
                     rprint(
                         "\nGemini API key [bold red]not found[/bold red]. Please add your API key first.\n"
                     )
-            case 2:
+            case "2":
                 add_api_key.add_api_key()
                 rprint(
                     "\nYour Gemini API key has been added [bold green]successfully[/bold green].\n"
                 )
             case _:
-                print("\nInvalid selection. Please enter 1, 2, or 3.\n")
-        choice = int(input(menu_text))
+                rprint(
+                    "\n[bold red]Invalid sel-ection[/bold red]. Please enter 1, 2, or 3.\n"
+                )
+        choice = input(menu_text)
 
 
 if __name__ == "__main__":
